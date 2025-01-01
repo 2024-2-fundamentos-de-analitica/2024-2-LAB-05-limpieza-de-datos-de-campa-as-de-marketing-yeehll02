cd7396774383cdffa6d0c8f70f8cd55f32aa4569
yeehll02 <116982357+yeehll02@users.noreply.github.com>
2025-01-01 17:47:04 -0500

    LAB - 5

--- homework/homework.py
+++ homework/homework.py
@@ -4,6 +4,15 @@ Escriba el codigo que ejecute la accion solicitada.
 
 # pylint: disable=import-outside-toplevel
 
+import zipfile
+import pandas as pd
+
+
+def zip_to_df(zip, csv_name):
+    with zipfile.ZipFile(zip, 'r') as archivo_zip:
+        with archivo_zip.open(csv_name) as archivo_csv:
+            df = pd.read_csv(archivo_csv)
+    return df
 
 def clean_campaign_data():
     """
@@ -38,7 +47,7 @@ def clean_campaign_data():
     - previous_campaing_contacts
     - previous_outcome: cmabiar "success" por 1, y cualquier otro valor a 0
     - campaign_outcome: cambiar "yes" por 1 y cualquier otro valor a 0
-    - last_contact_day: crear un valor con el formato "YYYY-MM-DD",
+    - last_contact_day:tecrear un valor con el formato "YYYY-MM-DD",
         combinando los campos "day" y "month" con el año 2022.
 
     economics.csv:
@@ -46,10 +55,50 @@ def clean_campaign_data():
     - const_price_idx
     - eurobor_three_months
 
-
+    
 
     """
+    dic = {'files/input/bank-marketing-campaing-0.csv.zip':'bank_marketing_0.csv', 
+           'files/input/bank-marketing-campaing-1.csv.zip':'bank_marketing_1.csv',
+           'files/input/bank-marketing-campaing-2.csv.zip':'bank_marketing_2.csv',
+           'files/input/bank-marketing-campaing-3.csv.zip':'bank_marketing_3.csv',
+           'files/input/bank-marketing-campaing-4.csv.zip':'bank_marketing_4.csv',
+           'files/input/bank-marketing-campaing-5.csv.zip':'bank_marketing_5.csv', 
+           'files/input/bank-marketing-campaing-6.csv.zip':'bank_marketing_6.csv',
+           'files/input/bank-marketing-campaing-7.csv.zip':'bank_marketing_7.csv',
+           'files/input/bank-marketing-campaing-8.csv.zip':'bank_marketing_8.csv',
+           'files/input/bank-marketing-campaing-9.csv.zip':'bank_marketing_9.csv'}  
 
+    dataframes = []
+    for zip, csv in dic.items():
+        df = zip_to_df(zip, csv)
+        dataframes.append(df)
+    df_combinado = pd.concat(dataframes, ignore_index=False)
+    df_combinado['month'] = pd.to_datetime(df_combinado['month'], format='%b').dt.month
+
+    # client    
+    client = df_combinado[['client_id', 'age', 'job', 'marital', 'education', 'credit_default', 'mortgage']].copy()
+    client['job'] = client['job'].str.replace('.', '').str.replace('-', '_')
+    client['education'] = client['education'].str.replace('.', '_').replace('unknown', pd.NA)
+    client['credit_default'] = client['credit_default'].map({'yes': 1}).fillna(0)
+    client['mortgage'] = client['mortgage'].map({'yes': 1}).fillna(0)
+    client.to_csv('files/output/client.csv', index=False)
+
+    # campaign
+    campaign = df_combinado[['client_id', 'number_contacts', 'contact_duration', 'previous_campaign_contacts', 'previous_outcome', 'campaign_outcome']].copy()  
+    campaign['previous_outcome']= campaign['previous_outcome'].map({'success': 1}).fillna(0)
+    campaign['campaign_outcome']= campaign['campaign_outcome'].map({'yes': 1}).fillna(0)
+    campaign['last_contact_date']= pd.to_datetime('2022-' + df_combinado['month'].astype(str) + '-' + df_combinado['day'].astype(str) , format='%Y-%m-%d')
+    # campaign['last_contact_date']= '2022-' + df_combinado['day'].astype(str) + '-' + df_combinado['month'].astype(str)
+    campaign.to_csv('files/output/campaign.csv', index=False)
+
+    print(campaign['last_contact_date'].value_counts())
+
+    # economics
+    economics = df_combinado[['client_id', 'cons_price_idx', 'euribor_three_months']].copy()    
+    economics.to_csv('files/output/economics.csv', index=False)
+
+    
     return
 
 
